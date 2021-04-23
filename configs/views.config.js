6507cff1c76b3f6661b97ae3f1a618cadddb7494
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-23 18:57:13 +0200

    Create navbar and footer layout, also style

--- configs/views.config.js
+++ configs/views.config.js
@@ -5,6 +5,6 @@ const path = require('path');
 module.exports = (app) => {
     app.set("../views", path.join(__dirname, "views"));
     app.set("view engine", "hbs");
-    app.use(express.static(path.join(__dirname, "public")));
+    app.use(express.static(path.join(__dirname, '..', "public")));
     //app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 }
