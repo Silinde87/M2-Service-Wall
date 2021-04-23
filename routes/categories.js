9a02859c9fd67a54971e2ded5e0fb19da92f47c4
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-23 23:55:52 +0200

    Create index and /categories/:id routes and views

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,17 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const { populate } = require("../models/Service.model");
+const router = express.Router();
+const Service = require("../models/Service.model");
+const User = require("../models/User.model");
 
-router.get('/', (req, res, next) => {
-    
+router.get("/:name", (req, res, next) => {
+	const { name } = req.params;
+	Service.find({ "category.name": name })
+		.populate("user_id")
+		.then((services) => {
+			res.render("categories", { services });
+		})
+		.catch((err) => console.error(err));
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const data = require("../bin/seeds/data");
+const categories = data.categories;
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+	res.render("index", { categories });
 });
 
 module.exports = router;
