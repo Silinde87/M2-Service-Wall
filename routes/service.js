eb01b6175a092c4d107a142ffcf49c2d46eddca4
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-24 18:26:22 +0200

    Style index, category and service list screen

--- routes/service.js
+++ routes/service.js
@@ -8,11 +8,10 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/search', (req, res, next) => {
-    const { description } = req.query;
-    Service.find({description:  {$regex : `.*${description}.*`}})
+    let { description } = req.query;
+    Service.find({description:  {$regex : `.*(?i)${description}.*`}})
         .populate("user_id")
         .then((services) => {
-            console.log(services);
             res.render("service/service-list", { services, description });
 		})
 		.catch((err) => console.error(err));
