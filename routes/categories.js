1df5ff9d950b41422d8e81e97b765eac027416c6
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-24 16:12:56 +0200

    Implement search bar and create views for listed services

--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { populate } = require("../models/Service.model");
 const router = express.Router();
 const Service = require("../models/Service.model");
 const User = require("../models/User.model");
@@ -9,9 +8,6 @@ router.get("/:name", (req, res, next) => {
 	Service.find({ "category.name": name })
 		.populate("user_id")
 		.then((services) => {
-			//todo: Get media rate from each service
-			console.log(services[0].user_id.reviews)
-			
 			res.render("categories", { services });
 		})
 		.catch((err) => console.error(err));
