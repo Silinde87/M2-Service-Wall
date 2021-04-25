051b767a297f7b25334d4ee2236a9800447b475b
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-25 11:41:56 +0200

    Create service/delete route

--- routes/service.js
+++ routes/service.js
@@ -5,7 +5,6 @@ const User = require("../models/User.model");
 const data = require("../bin/seeds/data");
 const categories = data.categories;
 const { isLoggedIn } = require("../middlewares/index");
-require("../middlewares/index");
 
 router.get("/", (req, res, next) => {});
 
@@ -20,12 +19,12 @@ router.get("/search", (req, res, next) => {
 		.catch((err) => console.error(err));
 });
 
-/* Create service routes */
+/* CREATE service routes */
 router.get("/create", isLoggedIn, (req, res, next) => {
 	res.render("service/service-create", { categories });
 });
 
-router.post("/create", (req, res, next) => {
+router.post("/create", isLoggedIn, (req, res, next) => {
 	let { description, price, category, location, image } = req.body;
 	const user_id = req.user._id;
 	//Get category object from category name.
@@ -36,16 +35,36 @@ router.post("/create", (req, res, next) => {
 	//temp. hardcoded location. todo: implement mapbox
 	location = ["35.6828387", "139.7594549"];
 	//image: todo, get "string" from file in a submit. Implement cloudinary
-	
-    //todo, add image property at create query
-    Service.create({ description, price, location, user_id, category })
-        .then(() => {            
-            res.redirect("/");
-            //todo: Remove redirect to index and redirect to user profile
-            //res.redirect(`/profile/${user_id}`);
-        })
-        .catch((err) => console.error(err));
-    
+
+	//todo, add image property at create query
+	Service.create({ description, price, location, user_id, category })
+		.then(() => {
+			res.redirect("/");
+			//todo: Remove redirect to index and redirect to user profile
+			//res.redirect(`/profile/${user_id}`);
+		})
+		.catch((err) => console.error(err));
+});
+
+/* DELETE service route */
+router.post("/:id/delete", isLoggedIn, (req, res, next) => {
+	const service_id = req.params.id;
+	const user_id = req.user._id;
+
+	Service.findById(service_id)
+		.then((service) => {
+			//Compare service_id and user_id, if equal deletes service.
+			if (JSON.stringify(service.user_id) === JSON.stringify(user_id)) {
+				Service.findByIdAndRemove(service_id)
+					.then(() => {
+						res.redirect("/");
+						//todo: Remove redirect to index and redirect to user profile
+						//res.redirect(`/profile/${user_id}`);
+					})
+					.catch((err) => console.error(err));
+			}
+		})
+		.catch((err) => console.error(err));
 });
 
 /* Profile view. Rendered by user id. */
