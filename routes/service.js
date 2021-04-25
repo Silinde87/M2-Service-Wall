31c91d06c0b3cfc85e7067135354dcb5a58ddf97
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-25 09:26:14 +0200

    Create specific service route and view. Also styled

--- routes/service.js
+++ routes/service.js
@@ -7,14 +7,27 @@ router.get('/', (req, res, next) => {
     
 });
 
+/* Search bar route. Search a service by description */
 router.get('/search', (req, res, next) => {
-    let { description } = req.query;
+    const { description } = req.query;
     Service.find({description:  {$regex : `.*(?i)${description}.*`}})
         .populate("user_id")
         .then((services) => {
             res.render("service/service-list", { services, description });
 		})
 		.catch((err) => console.error(err));
+})
+
+/* Profile view. Rendered by user id. */
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+    Service.findById(id)
+        .populate("user_id")
+        .then((service) => {
+            console.log(service)
+            res.render('service/service', service);
+        })
+        .catch((err) => console.error(err));
 })
 
 module.exports = router;
