0b371aaa5ecffc74ff6c3828b80da51414f562db
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-29 13:06:44 +0200

    fix login after signup

--- routes/auth.js
+++ routes/auth.js
@@ -29,9 +29,9 @@ router.post('/signup', (req, res, next) =>{
 
         User.create({ username, email, password: hashPassword })
         .then((newUser) => {
-            if({username}){
-                res.render('auth/signup', {errorMessage: "This username already exists"});
-            }
+            // if({username}){
+            //     res.render('auth/signup', {errorMessage: "This username already exists"});
+            // }
             console.log('USER CREATED', newUser) 
             req.login(newUser, (error) => {
                 //if(error) next(error);
